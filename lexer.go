d2177bd1af4af161871710c94b35b7eceb884813
alejandro06020 <alejandro.minda@epn.edu.ec>
2023-11-16 17:10:45 -0500

    Segunda parte

--- lexer.go
+++ lexer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"io"
+	"unicode"
 )
 
 type Token int
@@ -57,5 +58,58 @@ func NewLexer(reader io.Reader) *Lexer {
 	return &Lexer{
 		pos:    Position{line: 1, column: 0},
 		reader: bufio.NewReader(reader),
+	}
+}
+func (l *Lexer) Lex() (Position, Token, string) {
+	// keep looping until we return a token
+	for {
+		r, _, err := l.reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				return l.pos, EOF, ""
+			}
+
+			// at this point there isn't much we can do, and the compiler
+			// should just return the raw error to the user
+			panic(err)
+		}
+
+		// update the column to the position of the newly read in rune
+		l.pos.column++
+
+		switch r {
+		case '\n':
+			l.resetPosition()
+		case ';':
+			return l.pos, SEMI, ";"
+		case '+':
+			return l.pos, ADD, "+"
+		case '-':
+			return l.pos, SUB, "-"
+		case '*':
+			return l.pos, MUL, "*"
+		case '/':
+			return l.pos, DIV, "/"
+		case '=':
+			return l.pos, ASSIGN, "="
+		default:
+			if unicode.IsSpace(r) {
+				continue // nothing to do here, just move on
+			} else if unicode.IsDigit(r) {
+				// backup and let lexInt rescan the beginning of the int
+				startPos := l.pos
+				l.backup()
+				lit := l.lexInt()
+				return startPos, INT, lit
+			} else if unicode.IsLetter(r) {
+				// backup and let lexIdent rescan the beginning of the ident
+				startPos := l.pos
+				l.backup()
+				lit := l.lexIdent()
+				return startPos, IDENT, lit
+			} else {
+				return l.pos, ILLEGAL, string(r)
+			}
+		}
 	}
 }
