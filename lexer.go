7f45be43a24bc994a4f747209adaa9a81d034920
AlexMorvi <ariel.morales@epn.edu.ec>
2023-11-16 17:06:06 -0500

    Lexer parte 1
    alexander morales

---
+++ lexer.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"io"
+)
+
+type Token int
+
+const (
+	EOF = iota
+	ILLEGAL
+	IDENT
+	INT
+	SEMI // ;
+
+	// Infix ops
+	ADD // +
+	SUB // -
+	MUL // *
+	DIV // /
+
+	ASSIGN // =
+)
+
+var tokens = []string{
+	EOF:     "EOF",
+	ILLEGAL: "ILLEGAL",
+	IDENT:   "IDENT",
+	INT:     "INT",
+	SEMI:    ";",
+
+	// Infix ops
+	ADD: "+",
+	SUB: "-",
+	MUL: "*",
+	DIV: "/",
+
+	ASSIGN: "=",
+}
+
+func (t Token) String() string {
+	return tokens[t]
+}
+
+type Position struct {
+	line   int
+	column int
+}
+
+type Lexer struct {
+	pos    Position
+	reader *bufio.Reader
+}
+
+func NewLexer(reader io.Reader) *Lexer {
+	return &Lexer{
+		pos:    Position{line: 1, column: 0},
+		reader: bufio.NewReader(reader),
+	}
+}
